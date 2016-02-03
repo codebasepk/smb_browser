63a6ff554cec655490776464ca922b7333d6637b
scorpion1 <shgr8rb@gmail.com>
2016-02-03 16:11:08 +0500

    add hardcoded protocol

--- app/src/main/java/com/pits/smbbrowse/activities/LoginActivity.java
+++ app/src/main/java/com/pits/smbbrowse/activities/LoginActivity.java
@@ -3,6 +3,9 @@ package com.pits.smbbrowse.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,6 +18,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText mHostEntry;
     private EditText mUsernameEntry;
     private EditText mPasswordEntry;
+    private final String SMB_PROTOCOL = "smb://";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,30 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mHostEntry = (EditText) findViewById(R.id.host_address_entry);
         mUsernameEntry = (EditText) findViewById(R.id.username_entry);
         mPasswordEntry = (EditText) findViewById(R.id.password_entry);
+
+        mHostEntry.setText(SMB_PROTOCOL);
+        Selection.setSelection(mHostEntry.getText(), mHostEntry.length());
+        mHostEntry.requestFocus();
+        mHostEntry.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().startsWith(SMB_PROTOCOL)) {
+                    mHostEntry.setText(SMB_PROTOCOL);
+                    Selection.setSelection(mHostEntry.getText(), mHostEntry.length());
+                }
+            }
+        });
+
     }
 
     @Override
